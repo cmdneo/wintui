4cedc56b7c5f3a9c9e46475b0a06112e5fd621c0
Amiy Kumar <amiy90067@gmail.com>
2022-03-03 19:12:15 +0530

    Fix exit on esc

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <time.h>
 
 #include "wintui/wintui.h"
@@ -9,7 +10,8 @@ int main()
 	char buf[256] = { 0 };
 	int event = 0;
 	int run = 1;
-	point2D pos = { 0 };
+	int fg_col = 1;
+	point2D pos = { .x = 40, .y = 20 };
 
 	surface *s = surface_init(1);
 	if (NULL == s) {
@@ -21,19 +23,23 @@ int main()
 	wt_disable(TM_CURSOR);
 
 	while (run) {
-		int bytes = wt_read(buf, 256); /* LESS than 256 always */
-		buf[bytes - 1] = '\0';
+		int bytes = wt_read(buf, 255); /* LESS than 256 always */
+		buf[bytes] = '\0';
 		if ('\x1b' == buf[0] && 1 == bytes)
 			run = 0;
 
-		surface_set(s, TC_TURQUOISE, TC_BLACK, TF_BOLD | TF_UNDERLINE);
-		surface_draw(s, "X", pos.x, pos.y);
+		surface_set(s, fg_col, TC_BLACK, TF_BOLD | TF_UNDERLINE);
+		for (int i = 0; i < 50; i++) {
+			surface_draw(
+				s, "*",
+				2 * pos.x + 2 * (int)(10 * sin(2 * 3.14 *
+							       (i / 50.))),
+				pos.y + (int)(10 * cos(2 * 3.14 * (i / 50.))));
+		}
 
 		event = surface_update(s);
-		(void)(event = 2 * event);
-
-		pos.x = 3 * (rand() % s->tsize.x / 3);
-		pos.y = rand() % s->tsize.y;
+		fg_col++;
+		fg_col %= TC_COUNT;
 	}
 
 	/* INFO: As per relative sensetivity of colors to the human eye 
